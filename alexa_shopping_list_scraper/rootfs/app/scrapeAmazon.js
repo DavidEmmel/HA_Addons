e0e1f2fc8d9e94d65bf95e5fe45bf36678bb7d7a
DaveE <DavidEmmel@users.noreply.github.com>
2025-03-02 16:26:16 -0500

    Update scrapeAmazon.js

--- alexa_shopping_list_scraper/rootfs/app/scrapeAmazon.js
+++ alexa_shopping_list_scraper/rootfs/app/scrapeAmazon.js
@@ -75,7 +75,7 @@ async function getOTP(secret) {
         '--disable-extensions',
         '--disable-gpu',
         '--disable-dev-shm-usage',
-        '--user-agent Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0',
+        '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0"',
         '--disable-features=site-per-process'
                 ],
 //            args: ['--no-sandbox', '--disable-setuid-sandbox', '--single-process'],
